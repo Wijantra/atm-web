450a183c8a8298130b741e9db39c939eef235c56
Ding <ding533@hotmail.com>
2020-09-18 00:53:27 +0700

    add bankaccount

--- src/main/java/th/ac/ku/atm/service/CustomerService.java
+++ src/main/java/th/ac/ku/atm/service/CustomerService.java
@@ -3,14 +3,19 @@ package th.ac.ku.atm.service;
 import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 import th.ac.ku.atm.model.Customer;
-
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class CustomerService {
 
-    private ArrayList<Customer> customerList = new ArrayList<>();
+    private ArrayList<Customer> customerList;
+
+    @PostConstruct
+    public void postConstruct() {
+        this.customerList = new ArrayList<>();
+    }
 
     public void createCustomer(Customer customer) {
         String hashPin = hash(customer.getPin());
